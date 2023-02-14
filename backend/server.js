901e0229a78aaf5fc89937e3c4ee98e218e6b8ea
jerry1249756 <jerry1249756@gmail.com>
2023-02-14 17:27:18 +0800

    fix

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import router from "./src/api.js";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+//development
+/*
+app.use("/api", router); 
+*/
+
+//production
+app.use(express.static(path.join(process.cwd(), "../frontend/build")));
+app.use("/api", router);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "../frontend/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
+});
